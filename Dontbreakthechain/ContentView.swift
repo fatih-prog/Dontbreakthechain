ac74abc1f6befc8003c57e73268112de1caf11f0
FatihHSavas <fhsuyelik@gmail.com>
2025-12-30 22:19:59 +0300

    Initial Commit

---
+++ Dontbreakthechain/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Dontbreakthechain
+//
+//  Created by Fatih H. SAVAŞ on 30.12.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Dontbreakthechain/DontbreakthechainApp.swift
@@ -0,0 +1,17 @@
+//
+//  DontbreakthechainApp.swift
+//  Dontbreakthechain
+//
+//  Created by Fatih H. SAVAŞ on 30.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct DontbreakthechainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
